dc2f4531ea5476ce36d58af4217aab61c2b78edb
lasserre <cstew33@gmail.com>
2023-08-23 09:24:20 -0500

    Added __str__ and __eq__ to Location

--- src/varlib/location.py
+++ src/varlib/location.py
@@ -20,3 +20,23 @@ class Location:
         self.loc_type = loc_type
         self.reg_name = reg_name.lower()    # make register casing consistent
         self.offset = offset
+
+    def __eq__(self, __value: object) -> bool:
+        if isinstance(__value, Location):
+            if self.loc_type == LocationType.Register:
+                # ignore offset for register locations
+                return  self.loc_type == __value.loc_type and \
+                        self.reg_name == __value.reg_name
+            else:
+                # ignore reg name for memory locations
+                return  self.loc_type == __value.loc_type and \
+                        self.offset == __value.offset
+        return False
+
+    def __str__(self) -> str:
+        if self.loc_type == LocationType.Register:
+            return f'{self.reg_name}'
+        elif self.loc_type == LocationType.Stack:
+            return f'Stack[{self.offset:#x}]'
+        else:
+            return f'Mem[{self.offset:#x}]'
