ed47bda0852d6265ba5b61a8d163face5e24ee61
lasserre <cstew33@gmail.com>
2024-04-30 07:38:36 -0500

    Enable Decompiler Parameter ID

--- src/astlib/ghidra_scripts/set_analysis_options.py
+++ src/astlib/ghidra_scripts/set_analysis_options.py
@@ -19,3 +19,10 @@ dwarf_debug_item_limit = int(1e9)   # 1 billion - effectively no limit :)
 
 setAnalysisOption(currentProgram, 'DWARF.Debug Item Limit', str(dwarf_debug_item_limit))
 print('Set DWARF.Debug Item Limit to {:,}'.format(dwarf_debug_item_limit))
+
+# CLS: this worked from the GUI python console...
+DECOMPILE_TIMEOUT_SEC = 240         # 4 min
+setAnalysisOption(currentProgram, 'Decompiler Parameter ID', 'True')
+setAnalysisOption(currentProgram, 'Decompiler Parameter ID.Analysis Decompiler Timeout (sec)', str(DECOMPILE_TIMEOUT_SEC))
+print('Enabled Decompiler Parameter ID')
+print('Set Decompiler Parameter ID.Analysis Decompiler Timeout (sec) to {:,}'.format(DECOMPILE_TIMEOUT_SEC))
