71aa506e8668d2fec139fd1cb190fc7c6d5b3c37
lasserre <cstew33@gmail.com>
2024-12-28 11:03:10 -0600

    Updated location names to match Ghidra

--- src/varlib/location.py
+++ src/varlib/location.py
@@ -1,13 +1,13 @@
 from typing import List
 
 class LocationType:
-    Register = 'reg'
+    Register = 'register'
     Stack = 'stack'
     Memory = 'mem'
     # these may be temporary...going to see how these correlate
     # with DWARF symbols first
-    Join = 'JOIN'
-    Unique = 'UNIQUE'
+    Join = 'join'
+    Unique = 'unique'
     # this is intended to represent a location that doesn't exist or is unspecified
     # (by DWARF usually), I'm adding it for variables that have no location.
     # Looking at a variable like this, I think it got optimized out. So the debug
